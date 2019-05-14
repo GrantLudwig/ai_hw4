3f82d551dd7d480da7116abb28419ac980b92eb8
unknown <grantludwig97@gmail.com>
2019-05-13 22:46:20 -0700

    backtracking working, clean up code

--- hw4.py
+++ hw4.py
@@ -5,6 +5,7 @@
 #assumes board in 9x9
 
 import sys
+import copy
 
 boardSize = 9
 
@@ -58,21 +59,21 @@ def pruneDomain(domains, board):
                 if int(board[rowI][key[1]]) in domainList:
                     domainList.remove(int(board[rowI][key[1]]))
             #square
-            rowMod = key[0] % 8
-            colMod = key[1] % 8
+            rowM = 8 - key[0]
+            colM =8 - key[1]
             rowRange = None
             colRange = None
                 #find row range
-            if rowMod > 5:
+            if rowM > 5:
                 rowRange = (0,2)
-            elif rowMod > 2:
+            elif rowM > 2:
                 rowRange = (3,5)
             else:
                 rowRange = (6,8)
                 #find col range
-            if colMod > 5:
+            if colM > 5:
                 colRange = (0,2)
-            elif colMod > 2:
+            elif colM > 2:
                 colRange = (3,5)
             else:
                 colRange = (6,8)
@@ -81,12 +82,88 @@ def pruneDomain(domains, board):
                     if int(board[rowI][colI]) in domainList:
                         domainList.remove(int(board[rowI][colI]))
 
+def goodValue(domains, space, value):
+    row, col = space
+    for rowI in range(boardSize):
+        if row != rowI and value in domains[(rowI, col)]:
+            if len(domains[(rowI, col)]) > 1:
+                domains[(rowI, col)].remove(value)
+            else:
+                return False
+    for colI in range(boardSize):
+        if col != colI and value in domains[(row, colI)]:
+            if len(domains[(row, colI)]) > 1:
+                domains[(row, colI)].remove(value)
+            else:
+                return False
+    rowM = 8 - row
+    colM = 8 - col
+    rowRange = None
+    colRange = None
+        #find row range
+    if rowM > 5:
+        rowRange = (0,2)
+    elif rowM > 2:
+        rowRange = (3,5)
+    else:
+        rowRange = (6,8)
+        #find col range
+    if colM > 5:
+        colRange = (0,2)
+    elif colM > 2:
+        colRange = (3,5)
+    else:
+        colRange = (6,8)
+    for rowI in range(rowRange[0],rowRange[1] + 1):
+        for colI in range(colRange[0],colRange[1] + 1):
+            if row != rowI and col != colI and value in domains[(rowI, colI)]:
+                if len(domains[(rowI, colI)]) > 1:
+                    domains[(rowI, colI)].remove(value)
+                else:
+                    return False
+    return True
+
+def backTrack(domains, space):
+    row, col = space
+    if col >= boardSize:
+        row = row + 1
+        col = 0
+    if row >= boardSize:
+        return True, domains
+    #choose first in list
+    oldDict = copy.deepcopy(domains)
+    for value in domains[(row, col)]:
+        if goodValue(domains, (row,col), value):
+            domains[(row, col)] = [value]
+            found, solution = backTrack(domains, (row,col+1))
+            if found:
+                return found, solution
+            else:
+                domains = copy.deepcopy(oldDict)
+        else:
+            domains = copy.deepcopy(oldDict)
+    return False, []
+
 #driver code
 fileName = sys.argv[1]
 board = readFile(fileName)
 print('\n')
+print('Initial Board')
+print('-------------', '\n')
 printBoard(board)
-domains = buildDomains(board)
-print(domains)
 print('\n')
+domains = buildDomains(board)
 pruneDomain(domains, board)
+found, solution = backTrack(domains, (0,0))
+if found:
+    print('There is a solution')
+    print('-------------------', '\n')
+    completeBoard = []
+    for rowI in range(boardSize):
+        rowList = []
+        for colI in range(boardSize):
+            rowList.append(str(solution[rowI, colI][0]))
+        completeBoard.append(rowList)
+    printBoard(completeBoard)
+else:
+    print('There is no solution')
